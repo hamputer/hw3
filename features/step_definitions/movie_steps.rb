61f9646f4b64224c9942c87242df02b8abf83492
saasbook <saasbook@saasbook.(none)>
2012-09-29 16:54:51 -0700

    a

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,6 +4,7 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+	 Movie.create!(movie)
   end
   flunk "Unimplemented"
 end
@@ -25,4 +26,31 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+	rating_list.split(',').each do |rating|
+		if uncheck
+			uncheck("ratings_"+rating)
+		else
+			check("ratings_"+rating)
+		end
+	end
 end
+
+Then /I should see all movies rated "(.*)"/ do |ratings|
+	Movie.find_all_by_rating(ratings).each do |movie|
+		assert page.has_content? movie.title
+	end
+end
+
+Then /I should see no movies rated "(.*)"/ do |ratings|
+	Movie.find_all_by_rating(ratings).each do |movie|
+		assert page.has_no_content? movie.title
+	end
+end
+
+Then /I should see all of the movies/ do
+	Movie.all.each do |movie|
+		assert page.has_content? movie.title
+	end
+end
+
+
