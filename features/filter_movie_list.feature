a335a7985bae857a6774174c453dd6e47e261682
saasbook <saasbook@saasbook.(none)>
2012-09-29 18:51:28 -0700

    b

--- features/filter_movie_list.feature
+++ features/filter_movie_list.feature
@@ -23,13 +23,21 @@ Background: movies have been added to database
   
 Scenario: restrict to movies with 'PG' or 'R' ratings
   # enter step(s) to check the 'PG' and 'R' checkboxes
+	When I check the following ratings: PG,R
   # enter step(s) to uncheck all other checkboxes
+	And I uncheck the following ratings: G,PG-13,NC-17
   # enter step to "submit" the search form on the homepage
+	And I press "Refresh"
   # enter step(s) to ensure that PG and R movies are visible
+	Then I should see all movies rated: PG,R
   # enter step(s) to ensure that other movies are not visible
+	And I should see no movies rated: G,PG-13,NC-17
 
 Scenario: no ratings selected
   # see assignment
 
 Scenario: all ratings selected
   # see assignment
+	When I check the following ratings: G,PG,PG-13,NC-17,R
+	When I press "Refresh"
+	Then I should see all of the movies
--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -6,7 +6,6 @@ Given /the following movies exist/ do |movies_table|
     # you should arrange to add that movie to the database here.
 	 Movie.create!(movie)
   end
-  flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -15,7 +14,7 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.content  is the entire content of the page as a string.
-  flunk "Unimplemented"
+	page.body.index(e1) < page.body.index(e2)
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -28,28 +27,32 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
 	rating_list.split(',').each do |rating|
 		if uncheck
-			uncheck("ratings_"+rating)
+			step %Q{I uncheck "ratings_#{rating}"}
 		else
-			check("ratings_"+rating)
+			step %Q{I check "ratings_#{rating}"}
 		end
 	end
 end
 
-Then /I should see all movies rated "(.*)"/ do |ratings|
-	Movie.find_all_by_rating(ratings).each do |movie|
-		assert page.has_content? movie.title
+Then /I should see all movies rated: (.*)/ do |ratings|
+	ratings.split(',').each do |rating|
+		Movie.find_all_by_rating(rating).each do |movie|
+			assert page.has_content?(movie.title)
+		end
 	end
 end
 
-Then /I should see no movies rated "(.*)"/ do |ratings|
-	Movie.find_all_by_rating(ratings).each do |movie|
-		assert page.has_no_content? movie.title
+Then /I should see no movies rated: (.*)/ do |ratings|
+	ratings.split(',').each do |rating|
+		Movie.find_all_by_rating(rating).each do |movie|
+			assert page.has_no_content?(movie.title)
+		end
 	end
 end
 
 Then /I should see all of the movies/ do
 	Movie.all.each do |movie|
-		assert page.has_content? movie.title
+		assert page.has_content?(movie.title)
 	end
 end
 
